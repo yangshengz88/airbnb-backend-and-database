764ac4e7ab7ffba79e485eca188ce38ca4a6a415
yangshengz88 <yz4007@columbia.edu>
2025-01-29 14:59:39 +0800

    fix error for daphne in docker-compose.prod.yml

--- airbnb-backend/airbnb_backend/settings.py
+++ airbnb-backend/airbnb_backend/settings.py
@@ -8,7 +8,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
