83af2ed3516effe18866d4eb9c4ce06f1a3d1b9a
yangshengz88 <yz4007@columbia.edu>
2026-01-26 00:48:09 -0500

    update ip address

--- airbnb-backend/airbnb_backend/settings.py
+++ airbnb-backend/airbnb_backend/settings.py
@@ -24,7 +24,7 @@ if DEBUG:
 
     WEBSITE_URL = 'http://localhost:8000'
 else:
-    WEBSITE_URL = 'http://98.80.141.113:1337'
+    WEBSITE_URL = 'http://34.200.218.185:1337'
 
 CHANNEL_LAYERS = {
     'default': {
@@ -62,24 +62,24 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
-    'http://98.80.141.113:1337',
-    'http://98.80.141.113'
+    'http://34.200.218.185:1337',
+    'http://34.200.218.185'
 
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
-    'http://98.80.141.113:1337',
-    'http://98.80.141.113'
+    'http://34.200.218.185:1337',
+    'http://34.200.218.185'
 
 ]
 
 CORS_ORIGINS_WHITELIST = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
-    'http://98.80.141.113:1337',
-    'http://98.80.141.113'
+    'http://34.200.218.185:1337',
+    'http://34.200.218.185'
 ]
 
 
